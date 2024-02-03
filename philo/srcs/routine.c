6f1aedb9f0760f64e4a19b50257cb2e205edc389
Tomas Figueroa Estrada <tfiguero@car4s3.42barcelona.com>
2024-02-03 16:30:31 +0100

    joan banan

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: tfiguero <tfiguero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 21:58:31 by tfiguero          #+#    #+#             */
-/*   Updated: 2024/01/31 19:28:14 by tfiguero         ###   ########.fr       */
+/*   Updated: 2024/02/03 16:29:21 by tfiguero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_routine_multiple(t_philo *philo, long time_eat, long time_sleep)
 		philo->data->philos_full++;
 		pthread_mutex_unlock(&philo->data->mtx_philo_full);
 	}
-	philo->time_last_eat = ft_get_time(philo->data->time_start) + time_eat;
+	philo->time_last_eat = ft_get_time(philo->data->time_start);
 	pthread_mutex_unlock(&philo->mtx_eat);
 	ft_usleep(time_eat);
 	pthread_mutex_unlock(&philo->right_fork);
@@ -46,7 +46,6 @@ void	*ft_routine(void *arg)
 	
 	philo = arg;
 	finished = 0;
-	time_eat = 0;
 	pthread_mutex_lock(&philo->data->mtx_data);
 	time_eat = philo->data->time_to_eat;
 	time_sleep = philo->data->time_to_sleep;
@@ -55,6 +54,14 @@ void	*ft_routine(void *arg)
 		ft_usleep(philo->data->time_to_eat/10);
 	while(finished == 0)
 	{
+		if(philo->data->total_philo == 1)
+		{
+			pthread_mutex_lock(&philo->right_fork);
+			ft_print_st("taking the right fork", philo);
+			ft_usleep(philo->data->time_to_die);
+			pthread_mutex_unlock(&philo->right_fork);
+			break;
+		}
 		ft_routine_multiple(philo, time_eat, time_sleep);
 		pthread_mutex_lock(&philo->data->mtx_finished);
 		finished = philo->data->finished;
